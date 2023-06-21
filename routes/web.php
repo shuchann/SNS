fc60011a3391da113ddac1738b3add98e3ad8a95
shuchann <g021c1023@g.neec.ac.jp>
2023-06-21 22:08:10 +0900

    composer

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/a', [PostController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
