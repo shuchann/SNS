4f99c4cf38ca3591d538616c8aca28fdb7a8c155
shuchann <g021c1023@g.neec.ac.jp>
2023-06-23 18:33:15 +0900

    cssの変更

--- resources/views/postslist/index.blade.php
+++ resources/views/postslist/index.blade.php
@@ -3,18 +3,30 @@
 <head>
     <meta charset="utf-8">
     <title>みんなの投稿</title>
+    <header class="header">SHUTOOS</header>
+    <link rel="stylesheet" href="/css/index.css">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 </head>
     <body>
-        <h1>Blog Name</h1>
+        <h1>投稿一覧</h1>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            
+            <x-dropdown-link :href="route('logout')"
+                onclick="event.preventDefault();
+                this.closest('form').submit();">
+                {{ __('Log Out') }}
+            </x-dropdown-link>
+        </form>
+
         <div>
             <form action="{{ route('postslist.index') }}" method="GET">
                 <input type="text" name="keyword" value="{{ $keyword }}" placeholder="検索ワードを入力">
                 <input type="submit" value="検索">
             </form>
         </div>
-        <a href='/posts/create'>新規投稿</a>
+        <button type="button" onclick="location.href='{{ route('create') }}' ">新規投稿</button>
         <div class='posts'>
             @forelse ($posts as $post)
                 <div class='post'>
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,24 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{post}', [PostController::class ,'show'])
-    ->name('postslist.show');
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/dashboard', function(){
+       return view('dashboard'); 
+    })->name('dashboard');
+    
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/posts/create', [PostController::class, 'create'])->name('create');
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('/posts/{post}', [PostController::class ,'show'])->name('postslist.show');
+    Route::get('/', [PostController::class, 'index'])->name('postslist.index');
+    Route::put('/posts/{post}', [PostController::class, 'update']);
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+    Route::delete('/posts/{post}', [PostController::class,'delete']);
+});
 
-Route::get('/', [PostController::class, 'index'])
-    ->name('postslist.index');
-
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-Route::delete('/posts/{post}', [PostController::class,'delete']);
 require __DIR__.'/auth.php';
