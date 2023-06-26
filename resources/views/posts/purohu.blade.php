38a2d6dfb5b60210948603901a277e7c05eed87f
shuchann <g021c1023@g.neec.ac.jp>
2023-06-26 23:32:36 +0900

    プロフィール画面などのcss変更

--- resources/views/posts/purohu.blade.php
+++ resources/views/posts/purohu.blade.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <title>みんなの投稿</title>
-    <header class="header">
-    <h1 class="header_title">SHUTOOS</h1>
-    <button type="button" class="button" onclick="location.href='{{ route('create') }}' ">NEW POSTS</button>
-    <form method="POST" action="{{ route('logout') }}">
-            @csrf
-        <button type="submit" class="button">LOG OUT</button>
-        </form>
-    </header>
-    <link rel="stylesheet" href="/css/purohu.css">
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-</head>
-    <body>
-        {{ $user->name }}
+    <head>
+        <meta charset="utf-8">
+        <title>プロフィール</title>
+        <!--ヘッダー表示-->
+        <header class="header">
+        <h1 class="header_title">SHUTOOS</h1>
+        <button type="button" class="button" onclick="location.href='{{ route('create') }}' ">NEW POSTS</button>
+        <button type="button" class="button" onclick="location.href='{{ route('purohu') }}'">PROFILE</button>
+        <form method="POST" action="{{ route('logout') }}">
+                @csrf
+            <button type="submit" class="button">LOG OUT</button>
+            </form>
+        </header>
         
-        <h1 class="title">POSTS LIST</h1>
+        <!--リンク集-->
+        <link rel="stylesheet" href="/css/purohu.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="title">{{ $user->name }} PROFILE</h1>
         <div class='posts'>
             @forelse ($user->posts as $post)
                 <div class='post'>
@@ -26,11 +28,14 @@
                         <a href="/posts/{{ $post->id }}">POSTS NAME：{{ $post->title }}</a>
                     </h2>
                     <p class='body'>{{ $post->body }}</p>
-                    <img src="{{ $post->image_url }}" alt="">
+                    <img class="img" src="{{ $post->image_url }}" alt="">
                 </div>
             @empty
-                <p>関連投稿がありません。</p>
+                <p>投稿がありません</p>
             @endforelse
         </div>
+        <footer>
+            <button type="submit" class="button" onclick="location.href='{{ route('postslist.index') }}'">RETURN</button>
+        </footer>
     </body>
 </html>
